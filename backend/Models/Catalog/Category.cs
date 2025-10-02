ac9244b50dda8c35fce6d9cce2e984b8acc600ec
Nektarios <NektariosZampetoulakis@outlook.com>
2025-10-02 00:16:13 -0400

    Added unit testing

--- backend/Models/Catalog/Category.cs
+++ backend/Models/Catalog/Category.cs
@@ -3,7 +3,7 @@ namespace backend.Models;
 public class Category
 {
     public int Id { get; set; }           // Primary key
-    public string Name { get; set; } = ""; 
+    public string Name { get; set; } = "";
 
     // Navigation property: one category has many items
     public ICollection<Item> Items { get; set; } = new List<Item>();
