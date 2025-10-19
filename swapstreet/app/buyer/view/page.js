6afa7b4cb8b2e2d36f8eb92e2cb4fbbc5c05ee8a
William Charron-Boyle <williamcharronboyle@gmail.com>
2025-10-18 20:56:05 -0400

    (GH-38) feat: display item images

--- swapstreet/app/buyer/view/page.js
+++ swapstreet/app/buyer/view/page.js
@@ -11,7 +11,11 @@ import {
 
 const testItem = {
   name: "Name",
-  image: ["image1", "image2", "image3"],
+  image: [
+    "https://picsum.photos/id/233/800/500", 
+    "https://picsum.photos/id/21/600/1700", 
+    "https://picsum.photos/id/125/1300/700"
+  ],
   condition: "brand-new",
   category: "Shirt",
   location: "Sherbrooke",
@@ -21,52 +25,86 @@ const testItem = {
 };
 
 const testNavBar = <div>
-    <div className="h-12 bg-green-700"></div>
-    <div className="h-12 bg-green-500 text-white content-center px-5">
+  <div className="h-12 bg-green-700"></div>
+  <div className="h-12 bg-green-500 text-white content-center px-5">
     <div>Temporary Nav Bar</div>
   </div>
 </div>;
 
-const missingImage = <div className="flex flex-row bg-stone-200  justify-center items-center h-full">
-  <Image className="text-stone-400"/>
-</div>
-
-function ImageButton(icon) {
+function ImageButton(props) {
   return <div className="flex-none content-center">
-    <Button className="content-center mx-6">
+    <Button 
+      className="content-center mx-6"
+      onClick={props.callback}
+    >
       <div className="content-center">
-        {icon}
+        {props.icon}
       </div>
     </Button>
   </div>
 }
 
-function ImageView() {
-  return <div className="w-full h-full">
-    <div className="flex flex-col grow h-full">
-      <div className="h-full flex flex-row">
-        {ImageButton(<ChevronLeft/>)}
-        <div className="content-center py-20 w-full h-full">
-          {missingImage}
+class ImageView extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.urlList = props.urlList;
+    this.urls = props.urlList.filter(Boolean).length; 
+    this.state = {index: 0};
+    return;
+  }
+  
+  // Bind `this` to `right`.
+  right = () => {
+    this.setState((prev) => ({
+      index: (prev.index+1) % this.urls
+    }));
+    return;
+  }
+  
+  // Bind `this` to `left`.
+  left = () => {
+    this.setState((prev) => ({
+      index: (prev.index+(this.urls-1)) % this.urls
+    }));
+    return;
+  }
+  
+  render() {
+    return <div className="w-full h-full">
+      <div className="flex flex-col grow h-full">
+        <div className="h-full flex flex-row">
+          <ImageButton 
+            icon={<ChevronLeft/>} 
+            callback={this.left}
+          />
+          <div className="relative w-full h-full">
+            {/*Assume that the image is valid*/}
+            <img
+              src={this.urlList[this.state.index]}
+              className="absolute max-h-full inset-0 m-auto"
+            />
+          </div>
+          <ImageButton 
+            icon={<ChevronRight/>} 
+            callback={this.right}
+          />
         </div>
-        {ImageButton(<ChevronRight/>)}
-      </div>
-      <div className="h-32 bg-stone-300">
-        Footer
+        <div className="h-32 bg-stone-300">
+          Footer
+        </div>
       </div>
     </div>
-  </div>
+  }
 }
 
 export default function View() {
-  
-  
   return <div className="h-screen w-screen">
     <div className="flex flex-col h-full w-full">
       {testNavBar}
       <div className="flex grow">
         <div className="w-3/5 bg-stone-100">
-          {ImageView()}
+          <ImageView urlList={testItem.image}/>
         </div>
         <div className="w-2/5 bg-stone-200 min-w-60">
           Properties
