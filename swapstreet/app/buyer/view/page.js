6510cb212d99a1cd36e6117efb2d4fbb83cd0747
William Charron-Boyle <williamcharronboyle@gmail.com>
2025-10-26 14:21:52 -0400

    (GH-38) feature: hide arrows for single images

--- swapstreet/app/buyer/view/page.js
+++ swapstreet/app/buyer/view/page.js
@@ -9,21 +9,6 @@ import {
   Image
 } from "lucide-react";
 
-const testItem = {
-  name: "Name",
-  image: [
-    "https://picsum.photos/id/233/800/500", 
-    "https://picsum.photos/id/21/600/1700", 
-    "https://picsum.photos/id/125/1300/700"
-  ],
-  condition: "brand-new",
-  category: "Shirt",
-  location: "Sherbrooke",
-  price: "10.99 CAD",
-  brand: "Addidas",
-  transcription: "Made in China"
-};
-
 const testNavBar = <div>
   <div className="h-12 bg-green-700"></div>
   <div className="h-12 bg-green-500 text-white content-center px-5">
@@ -56,6 +41,8 @@ class ImageView extends React.Component {
     this.urlList = props.urlList;
     this.urls = props.urlList.filter(Boolean).length; 
     this.state = {index: 0};
+    this.renderer = this.urls == 1 ? this.renderSingle 
+      : this.renderMultiple;
     return;
   }
   
@@ -75,25 +62,36 @@ class ImageView extends React.Component {
     return;
   }
   
+  renderSingle = () => {
+    return (
+      <div className="relative w-full m-4">
+        {/*Assume that the image is valid*/}
+        <img
+          src={this.urlList[this.state.index]}
+          className="absolute max-h-full inset-0 m-auto"
+        />
+      </div>);
+  }
+  
+  renderMultiple = () => {
+    return (<div className="h-full w-full flex flex-row">
+      <ImageButton 
+        icon={<ChevronLeft/>} 
+        callback={this.left}
+      />
+        {this.renderSingle()}
+      <ImageButton 
+        icon={<ChevronRight/>} 
+        callback={this.right}
+      />
+    </div>);
+  }
+  
   render() {
     return <div className="w-full h-full">
       <div className="flex flex-col grow h-full">
         <div className="h-full flex flex-row">
-          <ImageButton 
-            icon={<ChevronLeft/>} 
-            callback={this.left}
-          />
-          <div className="relative w-full m-4">
-            {/*Assume that the image is valid*/}
-            <img
-              src={this.urlList[this.state.index]}
-              className="absolute max-h-full inset-0 m-auto"
-            />
-          </div>
-          <ImageButton 
-            icon={<ChevronRight/>} 
-            callback={this.right}
-          />
+          {this.renderer()}
         </div>
         <div className="h-32 bg-stone-300">
           Footer
@@ -134,31 +132,45 @@ export default function View() {
         
         /*Assume that the object defines all the item's attributes. 
         That is, none of the attributes are undefined.*/
-        setInfo(<div className="w-full h-full p-4">
-          <div className="w-full">
-            <div>
-              <div className="font-bold text-2xl">
-                {item.title}
+        setInfo(<div className="flex grow h-full">
+          <div className="w-3/5 bg-stone-100">
+            <ImageView urlList={new Array(item.imageUrl)}/>
+          </div>
+          <div className="w-2/5 bg-stone-200 min-w-60">
+            <div className="w-full bg-stone-200 min-w-60">
+              <div className="w-full h-full p-4">
+                <div className="w-full">
+                  <div>
+                    <div className="font-bold text-2xl">
+                      {item.title}
+                    </div>
+                    {item.description}
+                    <br/>
+                    <span className="font-bold text-2xl">
+                      CAD ${item.price}
+                    </span>
+                  </div>
+                  <br/>
+                  <div>
+                    <span className="font-bold">
+                      Condition:
+                    </span>
+                    {' ' + item.condition}
+                  </div>
+                </div>
+                <Separator/>
+                <div className="grid grid-cols-1 w-full">
+                  
+                  {/*Add links*/}
+                  <Button className="my-2">Buy Now</Button>
+                  <Button className="my-2">Add to Changing Room</Button>
+                </div>
+                <Separator/>
+                <div className="">
+                  Seller info
+                </div>
               </div>
-              {item.description}
-              <br/>
-              <span className="font-bold text-2xl">CAD ${item.price}</span>
             </div>
-            <br/>
-            <div>
-              <span className="font-bold">Condition: </span>{item.condition}
-            </div>
-          </div>
-          <Separator/>
-          <div className="grid grid-cols-1 w-full">
-            
-            {/*Add links*/}
-            <Button className="my-2">Buy Now</Button>
-            <Button className="my-2">Add to Changing Room</Button>
-          </div>
-          <Separator/>
-          <div className="">
-            Seller info
           </div>
         </div>);
         
@@ -175,23 +187,8 @@ export default function View() {
   return <div className="h-screen w-screen">
     <div className="flex flex-col h-full w-full">
       {testNavBar}
-      <div className="flex grow">
-        <div className="w-3/5 bg-stone-100">
-          <ImageView urlList={testItem.image}/>
-        </div>
-        <div className="w-2/5 bg-stone-200 min-w-60">
-          {
-            info ?? <div className={
-              "grid grid-cols-1 justify-items-center "
-              + "content-center w-full h-full"
-              }
-            >
-              <div className="w-20 h-20 text-center content-center">
-                Loading...
-              </div>
-            </div>
-          }
-        </div>
+      <div className="w-full h-full">
+        {info ?? ''}
       </div>
     </div>
   </div>
