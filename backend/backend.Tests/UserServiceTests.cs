f5b0fb64a35d26fafd3c9e0f74305757295ede42
nektarios <nektarioszampetoulakis@outlook.com>
2025-11-02 14:46:10 -0500

    updated UserService test

--- backend/backend.Tests/UserServiceTests.cs
+++ backend/backend.Tests/UserServiceTests.cs
@@ -89,7 +89,7 @@ namespace backend.Tests
             await _db.Users.AddAsync(user);
             await _db.SaveChangesAsync();
 
-            var result = await _service.LoginWithPasswordAsync(user, password);
+            var result = await _service.LoginWithPasswordAsync(user, hasher.HashPassword(password));
 
             result.Should().NotBeNull();
             result!.Email.Should().Be(user.Email);
