8b7dc442991bd35f33425025d7c8f3272473d545
Sawanoza <marc_a00@hotmail.com>
2025-11-08 23:36:00 -0500

    refactor: PostHog initialization

--- swapstreet/instrumentation-client.ts
+++ swapstreet/instrumentation-client.ts
@@ -1,9 +1,17 @@
 import posthog from "posthog-js";
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
-  ui_host: "https://us.posthog.com",
-  defaults: "2025-05-24",
-  capture_exceptions: true,
-  debug: process.env.NODE_ENV === "development",
-});
+// Only run PostHog if we have a key. This prevents polluting our analytics during tests/CICD
+const POSTHOG_KEY = process.env.NEXT_PUBLIC_POSTHOG_KEY?.trim();
+const POSTHOG_HOST = process.env.NEXT_PUBLIC_POSTHOG_HOST || "/ingest";
+
+if (POSTHOG_KEY) {
+  posthog.init(POSTHOG_KEY, {
+    api_host: POSTHOG_HOST,
+    ui_host: "https://us.posthog.com",
+    defaults: "2025-05-24",
+    capture_exceptions: true,
+    debug: process.env.NODE_ENV === "development",
+  });
+} else {
+  // Do nothing if the key is missing
+}
