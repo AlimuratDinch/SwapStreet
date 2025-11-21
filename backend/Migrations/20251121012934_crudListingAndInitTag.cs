d1c9e19eb3bc9dc8f221ae0f5a50b59f9b52434f
William Charron-Boyle <williamcharronboyle@gmail.com>
2025-11-20 21:05:13 -0500

    (GH-262) feature: add migrations for listings

---
+++ backend/Migrations/20251121012934_crudListingAndInitTag.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class crudListingAndInitTag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tags",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tags", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "listings",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    price = table.Column<int>(type: "integer", nullable: false),
+                    description = table.Column<string>(type: "character varying(32767)", maxLength: 32767, nullable: false),
+                    profile_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    tag_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_listings", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_listings_profiles_profile_id",
+                        column: x => x.profile_id,
+                        principalTable: "profiles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_listings_tags_tag_id",
+                        column: x => x.tag_id,
+                        principalTable: "tags",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_listings_profile_id",
+                table: "listings",
+                column: "profile_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_listings_tag_id",
+                table: "listings",
+                column: "tag_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "listings");
+
+            migrationBuilder.DropTable(
+                name: "tags");
+        }
+    }
+}
