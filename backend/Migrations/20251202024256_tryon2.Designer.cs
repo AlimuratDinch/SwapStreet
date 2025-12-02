f099f6a042035a89366dd3474adb475bf4f8db43
Nektarios <NektariosZampetoulakis@outlook.com>
2025-12-01 21:43:41 -0500

    Ran migration

--- backend/Migrations/20251202024256_tryon2.Designer.cs
+++ backend/Migrations/20251202024256_tryon2.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using backend.DbContexts;
@@ -11,9 +12,11 @@ using backend.DbContexts;
 namespace backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251202024256_tryon2")]
+    partial class tryon2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
