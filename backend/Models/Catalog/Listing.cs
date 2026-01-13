b8ac8e315e898e79c64ec8ad34202a3c3bc8f4d7
M-a-a-d-man <evanteboul@icloud.com>
2026-01-13 16:20:07 -0500

    fix/fixed namespace issue in backend

--- backend/Models/Catalog/Listing.cs
+++ backend/Models/Catalog/Listing.cs
@@ -2,7 +2,6 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using NpgsqlTypes;
 
-namespace backend.Models;
 public class Listing
 {
     // Primary Key
