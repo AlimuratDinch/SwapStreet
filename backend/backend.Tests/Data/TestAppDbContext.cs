41086962829a72d2eeaec3cd96101c13deb24fa4
M-a-a-d-man <evanteboul@icloud.com>
2026-01-14 01:10:46 -0500

    test/namespace github actions edit

--- backend/backend.Tests/Data/TestAppDbContext.cs
+++ backend/backend.Tests/Data/TestAppDbContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using backend.DbContexts;
 
-namespace backend.Tests.Data;
+
 
 public class TestAppDbContext(DbContextOptions<TestAppDbContext> options) : AppDbContext(options)
 {
