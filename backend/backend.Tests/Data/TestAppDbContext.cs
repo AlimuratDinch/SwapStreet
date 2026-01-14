3c8c9acf399ab348d729cbedeca29cc1b9cd4446
M-a-a-d-man <evanteboul@icloud.com>
2026-01-14 00:48:30 -0500

    fix/ran dotnet format

--- backend/backend.Tests/Data/TestAppDbContext.cs
+++ backend/backend.Tests/Data/TestAppDbContext.cs
@@ -3,10 +3,10 @@ using backend.DbContexts;
 
 namespace backend.Tests.Data;
 public class TestAppDbContext(DbContextOptions<TestAppDbContext> options) : AppDbContext(options)
-    {
+{
     protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Listing>().Ignore(l => l.SearchVector);
-        }
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Listing>().Ignore(l => l.SearchVector);
     }
+}
