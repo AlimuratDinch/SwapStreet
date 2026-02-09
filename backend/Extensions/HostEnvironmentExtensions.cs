5fef6605b1004a0e47bc909e00fd2571a6438875
Nektarios <NektariosZampetoulakis@outlook.com>
2026-02-09 18:49:56 -0500

    dotnet format

--- backend/Extensions/HostEnvironmentExtensions.cs
+++ backend/Extensions/HostEnvironmentExtensions.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using backend.Configuration;
+
 namespace backend.Extensions;
+
 public static class HostEnvironmentExtensions
 
 {
