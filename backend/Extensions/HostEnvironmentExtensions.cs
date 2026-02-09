c04f18b239d9c6a9f9603a03e8cd5a1afee33f5a
Nektarios <NektariosZampetoulakis@outlook.com>
2026-02-09 18:22:02 -0500

    Remove blank line in HostEnvironmentExtensions
    Remove an unnecessary blank line before the namespace declaration in backend/Extensions/HostEnvironmentExtensions.cs to clean up whitespace.

--- backend/Extensions/HostEnvironmentExtensions.cs
+++ backend/Extensions/HostEnvironmentExtensions.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using backend.Configuration;
-
 namespace backend.Extensions;
 public static class HostEnvironmentExtensions
 {
