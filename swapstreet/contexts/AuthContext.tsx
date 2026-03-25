b889bb04c47664555506caf5317f59756aa6f652
Nektarios <NektariosZampetoulakis@outlook.com>
2026-03-25 18:29:50 -0400

    update remove console.log

--- swapstreet/contexts/AuthContext.tsx
+++ swapstreet/contexts/AuthContext.tsx
@@ -121,7 +121,6 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     } catch (err) {
       console.warn("Silent refresh failed (User likely guest)");
     } finally {
-      console.log("authLoaded set to true");
       setAuthLoaded(true); // Finalize loading regardless of outcome
     }
   }, [API_URL, updateAuthState]);
